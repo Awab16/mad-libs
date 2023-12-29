166cf5c91fdfa094b7d0140af0f0597b2ac8711a
Awab16 <122179604+Awab16@users.noreply.github.com>
2023-12-29 20:26:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input('please enter your name: ')
+age = input('please enter your age:')
+color= input('please enter your favorite color:')
+job = input('please enter your job:')
+
+print('my name is '+ name)
+print('and my age is '+ age)
+print('my favorite color is '+ color)
+print('my job is '+ job)
